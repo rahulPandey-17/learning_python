60170700a3765de1e7789d65919feef4fb23a4f8
Rahul Pandey <0517rp.rahul@gmail.com>
2024-09-20 22:33:08 +0530

    variables and data-types

---
+++ Variables and Data Types/02.program.py
@@ -0,0 +1,30 @@
+# different types of data-types in python
+
+number = 17
+string = "Rahul Pandey"
+decimal = 88.25
+complexNumber = complex(10, 4)
+fruits = ["Apple", "Orange", "Guava", "Melon"]
+laptops = ("hp", "mac", "lenovo", "dell")
+myInfo = {"Name" : "Rahul Pandey", "Age" : 24, "Can Vote" : True}
+
+# printing the above values
+
+print(number)
+print(string)
+print(decimal)
+print(complexNumber)
+print(fruits)
+print(laptops)
+print(myInfo)
+
+# we can print the data-types of the above variables
+
+print("\nData-Types\n")
+print(type(number))
+print(type(string))
+print(type(decimal))
+print(type(complexNumber))
+print(type(fruits))
+print(type(laptops))
+print(type(myInfo))
