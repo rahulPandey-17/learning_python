d442891abb84c967371013885f976ca2d7b982a6
Rahul Pandey <0517rp.rahul@gmail.com>
2024-09-23 13:51:58 +0530

    loops

---
+++ Loops/10.program.py
@@ -0,0 +1,17 @@
+# for loop
+
+# name = "Rahul"
+
+# for characters in name :
+#     print(characters, end = " ")
+# print()
+
+# colors = ["Red", "Blue", "Grey", "Yellow"]
+
+# for color in colors :
+#     print(color)
+
+# range function
+
+for numbers in range(1, 11) :
+    print(numbers)
